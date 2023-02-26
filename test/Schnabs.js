5ddc31a7df380297a01c8a35cdadffaa2e9094f2
Ann Schnabel <anntschnabel@gmail.com>
2023-02-26 11:04:07 -0700

    setup: hardhat and nextjs

---
+++ test/Schnabs.js
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { ethers } = require("hardhat");
+
+describe("Auction Contract", function () {
+  let auction;
+  let token;
+  let nft;
+  const nftId = 0;
+
+  let deployer;
+  let addr1;
+  let addr2;
+  let addr3;
+  let addr4;
+  let addr5;
+
+  beforeEach(async function () {
+    [deployer, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
+
+    const Schnabs = await ethers.getContractFactory("Schnabs");
+    schnabs = await Schnabs.deploy();
+  });
+
+  describe("Deployments", function () {
+    it("Should deploy the contract to the network", async function () {
+      expect(schnabs.address).to.exist;
+    });
+  });
+});
