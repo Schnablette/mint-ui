0cd817d2cc5e1d818a0e243eba4ec51c7ea18db0
Ann Schnabel <anntschnabel@gmail.com>
2023-02-27 13:19:00 -0700

    button: basic button styles

--- components/atoms/button/Button.tsx
+++ components/atoms/button/Button.tsx
@@ -13,6 +13,8 @@ export interface ButtonProps {
   /** Purpose of button */
   onClick: () => undefined;
   /** Size of button */
+  outline?: boolean;
+  /** Size of button */
   rounded?: boolean;
   /** Size of button */
   size?: "sm" | "md" | "lg";
@@ -20,40 +22,105 @@ export interface ButtonProps {
   [x: string]: any;
 }
 
-const buttonStyles = cva(["font-bold"], {
-  variants: {
-    fullWidth: {
-      true: ["w-full"],
+const buttonStyles = cva(
+  ["font-bold", "ease-in-out", "transition-all", "duration-150", "box-border"],
+  {
+    variants: {
+      fullWidth: {
+        true: ["w-full"],
+      },
+      intent: {
+        primary: [
+          "hover:bg-purple-400",
+          "focus:outline-purple-200",
+          "focus:outline-offset-2",
+          "active:bg-purple-300",
+        ],
+        secondary: [
+          "hover:bg-ghost-300",
+          "focus:outline-ghost-300",
+          "focus:outline-offset-2",
+          "active:bg-ghost-200",
+          "text-text-500",
+        ],
+        danger: [
+          "hover:bg-red-300",
+          "focus:outline-red-300",
+          "focus:outline-offset-2",
+          "active:bg-red-200",
+        ],
+      },
+      outline: {
+        true: [""],
+        false: [""],
+      },
+      rounded: {
+        true: "rounded-full",
+        false: "rounded",
+      },
+      size: {
+        sm: ["px-3", "py-1.5", "text-sms"],
+        md: ["px-4", "py-2"],
+        lg: ["px-4", "py-2.5", "text-lg"],
+      },
     },
-    intent: {
-      primary: [
-        "bg-purple-500",
-        "hover:bg-purple-400",
-        "focus:outline-purple-200",
-        "focus:outline-offset-2",
-        "active:bg-purple-300",
-        "text-white",
-      ],
-      secondary: ["bg-ghost-900"],
-      danger: ["bg-red-500"],
+    compoundVariants: [
+      {
+        intent: "primary",
+        outline: false,
+        class: ["bg-purple-500", "text-white"],
+      },
+      {
+        intent: "primary",
+        outline: true,
+        class: [
+          "bg-transparent",
+          "ring-2",
+          "ring-inset",
+          "ring-purple-500",
+          "text-purple-500",
+          "hover:text-white",
+          "active:text-white",
+        ],
+      },
+      {
+        intent: "secondary",
+        outline: false,
+        class: ["bg-ghost-500"],
+      },
+      {
+        intent: "secondary",
+        outline: true,
+        class: ["bg-transparent", "ring-2", "ring-inset", "ring-ghost-500"],
+      },
+      {
+        intent: "danger",
+        outline: false,
+        class: ["bg-red-500", "text-white"],
+      },
+      {
+        intent: "danger",
+        outline: true,
+        class: [
+          "bg-transparent",
+          "ring-2",
+          "ring-inset",
+          "ring-red-500",
+          "text-red-500",
+          "hover:text-white",
+          "active:text-white",
+        ],
+      },
+    ],
+    defaultVariants: {
+      fullWidth: false,
+      intent: "primary",
+      outline: false,
+      rounded: false,
+      size: "md",
     },
-    rounded: {
-      true: "rounded-full",
-      false: "rounded",
-    },
-    size: {
-      sm: ["px-3", "py-1.5"],
-      md: ["px-4", "py-2"],
-      lg: ["px-4", "py-2.5"],
-    },
-  },
-  defaultVariants: {
-    fullWidth: false,
-    intent: "primary",
-    rounded: false,
-    size: "md",
-  },
-});
+  }
+);
 
 // interface Props extends ButtonProps, VariantProps<typeof buttonStyles> {}
 
@@ -63,12 +130,13 @@ export const Button = ({
   fullWidth = false,
   intent = "primary",
   onClick,
+  outline,
   rounded = false,
   size = "md",
   ...rest
 }: ButtonProps) => (
   <button
-    className={buttonStyles({ fullWidth, intent, rounded, size })}
+    className={buttonStyles({ fullWidth, intent, outline, rounded, size })}
     onClick={onClick}
   >
     {children}
--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -11,8 +11,10 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
+  children: "text",
   fullWidth: false,
   intent: "primary",
+  outline: false,
   rounded: false,
   size: "md",
 };
